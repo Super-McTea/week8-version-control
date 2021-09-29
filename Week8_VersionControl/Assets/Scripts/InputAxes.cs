861618557e3e3ef2319b5f95b090ae6806948599
Rhys Patton <rhys.patton@students.mq.edu.au>
2021-09-29 14:23:50 +1000

    player movement added

--- Week8_VersionControl/Assets/Scripts/InputAxes.cs
+++ Week8_VersionControl/Assets/Scripts/InputAxes.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayeMovement : MonoBehaviour
+public class InputAxes : MonoBehaviour
 {
+    public const string Horizontal = "Horizontal";
+    public const string Vertical = "Vertical";
+    
     // Start is called before the first frame update
     void Start()
     {
---
+++ Week8_VersionControl/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // [SerializedField]
+    public float movementSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dx = Input.GetAxis(InputAxes.Horizontal);
+        float dz = Input.GetAxis(InputAxes.Vertical);
+
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)) {
+            transform.Translate(movementSpeed * Vector3.forward * dz * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) {
+            transform.Translate(movementSpeed * Vector3.left * dx * Time.deltaTime);
+        }
+    }
+}
